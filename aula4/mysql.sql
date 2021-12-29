bea2f8d319df69862ba42efff6da05780c0d8479
fabio <fabioleonardo1995@gmail.com>
2021-12-29 16:14:54 -0300

    select

---
+++ aula4/mysql.sql
@@ -0,0 +1,19 @@
+select ano , nome, carga from cursos
+order by ano, nome;
+
+select nome, descricao, ano from cursos 
+where ano < 2016
+order by ano, nome;
+
+select nome, ano from cursos
+where ano between 2015 and 2018
+order by ano desc, nome asc;
+
+select nome, descricao, ano from cursos
+where ano in (2014, 2018)
+order by ano;
+
+select nome, carga, totaulas from cursos
+where carga > 35 and totaulas < 30;
+
+describe cursos;
